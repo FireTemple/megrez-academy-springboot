d91459e6241aa2a5c65fb15fc30923b5e61f9579
bohan <gk6511@wayne.edu>
2020-07-05 12:33:09 -0400

    init

---
+++ src/main/java/com/bohan/MegrezAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.bohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegrezAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegrezAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bohan/MegrezAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegrezAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
